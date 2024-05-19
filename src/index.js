7f83e21bde5577007fed7444bf5ab21de9828d7b
Irina Shibaeva <irina-kuppieva@yandex.ru>
2024-05-20 00:12:35 +0300

    bugs fixed

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import './css/style.css';
 import './js/app';
 
 function component() {
-    const element = document.createElement('div');
+  const element = document.createElement('div');
 
-    element.innerHTML = _.join();
-    element.classList.add('hello');
+  element.innerHTML = _.join();
+  element.classList.add('hello');
 
-    const myIcon = new Image();
-    myIcon.src = Icon;
+  const myIcon = new Image();
+  myIcon.src = Icon;
 
-    element.appendChild(myIcon);
+  element.appendChild(myIcon);
 
-    return element;
+  return element;
 }
 
 document.body.appendChild(component());
--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,15 @@
 const randomHole = document.querySelectorAll('.hole');
-let randomPosition = Math.floor(Math.random() * randomHole.length);
+const randomPosition = Math.floor(Math.random() * randomHole.length);
 let currentHole = randomHole[randomPosition];
 
 const character = document.querySelector('.hole_has-mole');
-  if (!currentHole.contains(character)) {
+if (!currentHole.contains(character)) {
   currentHole.appendChild(character);
 }
 
 function moveCharacter() {
-  let newRandomPosition = Math.floor(Math.random() * randomHole.length);
-  let newHole = randomHole[newRandomPosition];
+  const newRandomPosition = Math.floor(Math.random() * randomHole.length);
+  const newHole = randomHole[newRandomPosition];
 
   if (newHole !== currentHole && !newHole.contains(character)) {
     newHole.appendChild(character);

34bdc9d551cf942cc8a2d499f10a69dbac22076b
Irina Shibaeva <irina-kuppieva@yandex.ru>
2024-05-20 22:08:17 +0300

    updated5

--- src/index.js
+++ src/index.js
@@ -3,15 +3,9 @@ import './js/app';
 
 function component() {
   const element = document.createElement('div');
-
-  element.innerHTML = join();
   element.classList.add('hello');
-
   const myIcon = new Image();
-  //myIcon.src = Icon;
-
   element.appendChild(myIcon);
-
   return element;
 }
 

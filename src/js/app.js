33933f50f26675d2db4f63a6edf525a3b0946df1
Irina Shibaeva <irina-kuppieva@yandex.ru>
2024-05-27 22:11:40 +0300

    updated7

--- src/js/app.js
+++ src/js/app.js
@@ -13,11 +13,6 @@ const randomHole = document.querySelectorAll('.hole');
 const randomPosition = Math.floor(Math.random() * randomHole.length);
 let currentHole = randomHole[randomPosition];
 
-/*const character = document.querySelector('.hole_has-mole');
-if (!currentHole.contains(character)) {
-  currentHole.appendChild(character);
-}*/
-
 function moveCharacter() {
   const newRandomPosition = Math.floor(Math.random() * randomHole.length);
   const newHole = randomHole[newRandomPosition];
